5949bb04c1cc2dd81b2f3f5ede3c3bcb877127c0
Colisian McLeod <74551663+Colisian@users.noreply.github.com>
2024-12-09 07:47:34 -0500

    Test Script for activating winrm

---
+++ wsManTest.ps1
@@ -0,0 +1,21 @@
+$yourCred = Get-Credential domain\account
+$yourServer = "your.server.fqdn"
+
+$LatestThumb = Invoke-Command -ComputerName $yourServer `
+                            -Credential $yourCred `
+                            -ScriptBlock {
+                                Get-ChildItem -Path Cert:\LocalMachine\My |
+                                Where-Object {$_.subject -match "CN=$yourServer"} |
+                                Sort-Object -Property NotAfter |
+                                Select-Object -Last 1 -ExpandProperty Thumbprint
+                            }
+
+Set-WSManInstance -ResourceURI winrm/config/Listener `
+                  -SelectorSet @{Address="*";Transport="HTTPS"} `
+                  -ComputerName $yourServer `
+                  -Credential $yourCred `
+                  -ValueSet @{CertificateThumbprint=$LatestThumb}
+
+Invoke-Command -ComputerName $yourServer `
+               -Credential $yourCred `
+               -ScriptBlock { Restart-Service -Force -Name WinRM }
